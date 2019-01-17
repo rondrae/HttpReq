a5f91fc6116d467b13e58b9ad22a82f278323b8f
rondrae <rondrae@gmail.com>
2019-01-16 23:08:54 -0500

    Adrian to Fix on Create and create Methods

--- app/src/main/java/com/example/rondrae/httpreq/MainActivity.java
+++ app/src/main/java/com/example/rondrae/httpreq/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rondrae.httpreq;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,6 +13,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -22,19 +26,30 @@ import okhttp3.Response;
 
         public class MainActivity extends AppCompatActivity {
 
-            private TextView mText;
-            private ImageView image, image2, image3, etc;
+
 
             @Override
             protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_main);
-                mText = findViewById(R.id.textViewID);
+
+                final List<VideoModel> videoLists = new ArrayList<>();
+                final RecyclerView rView = findViewById(R.id.rID);
+
+                final PostAdapter userAdapter = new PostAdapter(videoLists);
+
+                rView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+                //rView.setAdapter(userAdapter);
+
+
+
+
 
 
                 OkHttpClient client = new OkHttpClient();
                //String site = "http://academyapptest.herokuapp.com/posts";
-                String site = "https://www.pornhub.com/webmasters/search?id=44bc40f3bc04f65b7a35&search=hard&tags[]=Teen&thumbsize=medium";
+                String site = "https://www.pornhub.com/webmasters/search?id=44bc40f3bc04f65b7a35&search=ebony&page=2&thumbsize=medium";
 
 
                 Request request = new Request.Builder()
@@ -65,8 +80,11 @@ import okhttp3.Response;
 
                                 for (int i = 0; i < videos.length(); i++) {
 
-                                    //CREATE AN OBJECT FROM THE MAIN OBJECT
+                                    //CREATE AN OBJECT FROM THE MAIN OBJECT CALLED videoObject
+
                                     JSONObject videoObject = videos.getJSONObject(i);
+                                    videoLists.add(new VideoModel(videoObject.getString("title"), videoObject.getString("duration"),
+                                            videoObject.getString("default_thumb") ));
                                     Log.d("good",videoObject.getString("default_thumb"));
 
                                 }
@@ -76,12 +94,12 @@ import okhttp3.Response;
                             }
 
 
-                            /*MainActivity.this.runOnUiThread(new Runnable() {
+                            MainActivity.this.runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    mText.setText(myResponse);
+                                    rView.setAdapter(userAdapter);
                                 }
-                            });*/
+                            });
 
                         }
                     }
--- app/src/main/java/com/example/rondrae/httpreq/PostAdapter.java
+++ app/src/main/java/com/example/rondrae/httpreq/PostAdapter.java
@@ -1,6 +1,65 @@
 package com.example.rondrae.httpreq;
 
-public class PostAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+
+    List<VideoModel>  videoList;
+
+    public PostAdapter(List<VideoModel> videoList) {
+        this.videoList = videoList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.video_layout, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        //CREATE ONE INSTANCE OF THE video
+        VideoModel video = videoList.get(i);
+        viewHolder.title.setText(video.getTitle());
+        viewHolder.description.setText(video.getDescription());
+        Picasso.get().load(video.getUrl()).into(viewHolder.thumbnail);
 
 
+    }
+
+    @Override
+    public int getItemCount() {
+        //RETURN THE SIZE OF THE LIST CREATED
+        return videoList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public  View mView;
+        private TextView title;
+        private TextView description;
+        private ImageView thumbnail;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.titleID);
+            description = itemView.findViewById(R.id.descriptionID);
+            thumbnail = itemView.findViewById(R.id.thumbNailID);
+
+
+
+        }
+    }
 }
